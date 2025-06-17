8426bfb93aad94c8092d2cb8746654ee026d6b94
Gabriel Dutra <dutragabriel507@gmail.com>
2025-06-17 11:41:15 -0300

    Add files via upload

---
+++ cadastro-tarefas-v3/backend/database/db.js
@@ -0,0 +1,18 @@
+// const { Sequelize } = require("sequelize");
+
+// const sequelize = new Sequelize({
+//   dialect: "sqlite",
+//   storage: "backend/database/tarefas.sqlite"
+// });
+
+// module.exports = sequelize;
+
+const {Sequelize} = require("sequelize");
+
+const sequelize = new Sequelize("cadastro_tarefas", "tarefas_user", "123456",{
+  host: "localhost",
+  dialect: "mysql",
+  timestamp: false,
+});
+
+module.exports = sequelize;
